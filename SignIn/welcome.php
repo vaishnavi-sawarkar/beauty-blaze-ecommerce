764b53d83e4361f63001109ea511cdcfcb3e5692
vaishnavi-sawarkar <vaishnavisawarkar25@gmail.com>
2026-06-30 23:04:06 +0530

    Add files via upload

---
+++ SignIn/welcome.php
@@ -0,0 +1,22 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body>
+<div class="container" style="background-color: lavender; align-items:center; height: 450px;">
+      <div style="text-align: center;  padding: 5px;margin-top: 10px; padding-top: 25px;  border-radius: 50%;;">
+
+      <img src="img\bb.webp"></div>
+        <i><h2 style="display:flex; text-align:center; justify-content:center; align-items:center; "> Congratulation !!! Login Successfully! <br> now blend ur beauty with Beauty Blaze</h2>
+           </i></div>
+          
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</body>
+</html>
